f35411b9e588d2db0a845810619cedea882f9279
Connor-Mawer <connormawer@gmail.com>
2025-05-08 12:28:39 +1000

    changes to try

--- Image_Processor/Processor.py
+++ Image_Processor/Processor.py
@@ -2,24 +2,17 @@ import cv2
 import numpy as np
 import rembg
 import time
-import webbrowser
 from svgpathtools import Path, Line, wsvg
 import lineGenerator
 import faceDetector
 import svgSaver
 
+def ImageProcessor():
+    cropped_face = faceDetector.detect_and_crop_face()
 
-cropped_face = faceDetector.detect_and_crop_face()
+    lineImage = lineGenerator.lineGenerator(cropped_face)
+    svg = svgSaver.save_as_svg(lineImage)
+    return svg
 
-lineImage = lineGenerator.lineGenerator(cropped_face)
-
-
-cv2.imshow('Face Detection', lineImage)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# ------- Convert lines to SVG format ---- WORK IN PROGRESSS
-# Convert lines to SVG format
-svgSaver.save_as_svg(lineImage, "output.svg")
-
-# ------- Trim lines to keep only points inside the face bounding box ---- WORK IN PROGRESSS
+svg = ImageProcessor()
 
